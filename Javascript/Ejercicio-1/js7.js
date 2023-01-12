e13375a9bb6a8ad664d525cbc73c74d3dc20789c
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-01-12 19:59:04 +0100

    d

---
+++ Javascript/Ejercicio-1/js7.js
@@ -0,0 +1,11 @@
+function oc_rojo() {
+    document.getElementById("rojo").style.display="none";
+}
+
+function oc_verde() {
+    document.getElementById("verde").style.display="none";
+}
+
+function oc_azul() {
+    document.getElementById("azul").style.display="none";
+}
