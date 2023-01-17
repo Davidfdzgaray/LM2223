21b7009fe49115331b2d3b23f920cb97155a5d5b
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-01-17 09:34:50 +0100

    s

--- Javascript/Ejercicio-2/ext_habitaciones.js
+++ Javascript/Ejercicio-2/ext_habitaciones.js
@@ -7,25 +7,42 @@ function consulta() {
     }
     else {
         if (adultos==1 && niños==0 || adultos==2 && niños==0) {
-            alert("Necesitará una habitación Doble. Se encuentra disponible en estos momentos.");
+            var aleatorio =  Math.random(); 
+            aleatorio = aleatorio * 100 + 1;
+            aleatorio = Math.trunc(aleatorio);
+
+            alert("Necesitará una habitación Doble. Hay " + aleatorio + " habitaciones dobles disponibles en estos momentos.");
         }
         else if (adultos==2 && niños==1) {
-            alert("Necesitará una habitación Doble con Supletoria. Se encuentra disponible en estos momentos.");
+            var aleatorio =  Math.random(); 
+            aleatorio = aleatorio * 100 + 1;
+            aleatorio = Math.trunc(aleatorio);
+
+            alert("Necesitará una habitación Doble con Supletoria. Hay " + aleatorio + " habitaciones dobles disponibles en estos momentos.");
         }
         else if (adultos==3 && niños==0) {
-            alert("Necesitará una habitación Triple. Se encuentra disponible en estos momentos.");
+            var aleatorio =  Math.random(); 
+            aleatorio = aleatorio * 100 + 1;
+            aleatorio = Math.trunc(aleatorio);
+
+            alert("Necesitará una habitación Triple. Hay " + aleatorio + " habitaciones dobles disponibles en estos momentos.");
         }
         else if (adultos==2 && niños==2) {
-            alert("Necesitará una habitación Familiar. Se encuentra disponible en estos momentos.");
+            var aleatorio =  Math.random(); 
+            aleatorio = aleatorio * 100 + 1;
+            aleatorio = Math.trunc(aleatorio);
+
+            alert("Necesitará una habitación Familiar. Hay " + aleatorio + " habitaciones dobles disponibles en estos momentos.");
         }
         else {
-            alert("Lo sentimos, no disponemos de habitaciones para el número de personas indicadas.");
+            alert("Lo sentimos, no disponemos de habitaciones para el número de personas indicadas. Recuerde que sólo podrá consultar por habitaciones de como máximo 4 personas en total.");
         }
     }        
 }
 
 
-/*switch (adultos,niños) {
+/*------------PRUEBAS-----------
+switch (adultos,niños) {
             case "1" && "0" || "2" && "0":
             alert("Necesitará una habitación Doble. Se encuentra disponible en estos momentos.");
             break;
