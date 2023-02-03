9b2543a9ac5ed1ad1d4f5f68df4f2261396b3f13
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-02-03 14:22:45 +0100

    er

---
+++ Javascript/revisiones/revisiones_js_2/ej2.js
@@ -0,0 +1,23 @@
+
+var mensajes = ["Mi primer tweet","LM es la mejor clase","Por fin Viernes!","Último tweet"];      
+
+function imprimir(){
+    for (var i=0;i<=mensajes.length-1;i++) {
+        document.write("<div>"+"<img src='images/pajaro.png'>"+mensajes[i]+"<img src='images/basura.png' onclick='borrar()' class='basura'>"+"</div>");
+    }
+}
+
+function borrar() {
+
+}
+
+function enviar() {
+
+}
+
+function ordenar() {
+
+}
+
+
+
