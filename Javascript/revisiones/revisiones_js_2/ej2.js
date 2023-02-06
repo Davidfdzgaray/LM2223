39b9156bcf8f41e63ad6f6f9b27a3a31226f7d80
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-02-06 10:27:11 +0100

    f

--- Javascript/revisiones/revisiones_js_2/ej2.js
+++ Javascript/revisiones/revisiones_js_2/ej2.js
@@ -1,18 +1,25 @@
 
 var mensajes = ["Mi primer tweet","LM es la mejor clase","Por fin Viernes!","Último tweet"];      
 
-function imprimir(){
+function imprimir() {
     for (var i=0;i<=mensajes.length-1;i++) {
-        document.write("<div>"+"<img src='images/pajaro.png'>"+mensajes[i]+"<img src='images/basura.png' onclick='borrar()' class='basura'>"+"</div>");
+        document.write("<div>"+"<img src='images/pajaro.png'>"+mensajes[i]+"<img src='images/basura.png' onclick='borrar("+i+")' class='basura'>"+"</div>");
+        
     }
 }
 
-function borrar() {
-
+function borrar(i) {
+    mensajes.splice(i, 1);
+    imprimir();
 }
 
 function enviar() {
-
+    var nmensaje = document.getElementById("mensaje").value;
+    if (nmensaje != "")
+    {
+        mensajes.push(nmensaje);
+        imprimir();
+    }
 }
 
 function ordenar() {
