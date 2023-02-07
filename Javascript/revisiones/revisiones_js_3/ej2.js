140659fb240f9e4dd9ce2c27898753eb096e6e06
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-02-07 11:30:48 +0100

    drg

---
+++ Javascript/revisiones/revisiones_js_3/ej2.js
@@ -0,0 +1,63 @@
+function reset() {
+    document.getElementById("azul").style.opacity= "0";
+    document.getElementById("rojo").style.opacity= "0";
+    document.getElementById("verde").style.opacity= "0";
+    document.getElementById("rosa").style.opacity= "0";
+    document.getElementById("amarillo").style.opacity= "0";
+    document.getElementById("naranja").style.opacity= "0";
+    document.getElementById("marron").style.opacity= "0";
+    document.getElementById("negro").style.opacity= "0";  
+    document.getElementById("morado").style.opacity= "0";      
+    document.getElementById("cian").style.opacity= "0";
+}
+
+function iniciar() {
+    reset();
+
+    var aleatorio =  Math.random(); 
+    aleatorio = aleatorio * 10 + 1;
+    aleatorio = Math.trunc(aleatorio);
+
+    /*var tiempo =  Math.random(); 
+    tiempo = tiempo * 2;
+    tiempo = tiempo * 1000;
+
+    setTimeout(tiempo)*/
+
+    switch (aleatorio) {
+        case 1:
+            document.getElementById("azul").style.opacity= "1";
+        break;
+        case 2:
+            document.getElementById("rojo").style.opacity= "1";
+        break;
+        case 3:
+            document.getElementById("verde").style.opacity= "1";
+        break;
+        case 4:
+            document.getElementById("rosa").style.opacity= "1";
+        break;
+        case 5:
+            document.getElementById("amarillo").style.opacity= "1";
+        break;
+        case 6:
+            document.getElementById("naranja").style.opacity= "1";
+        break;
+        case 7:
+            document.getElementById("marron").style.opacity= "1";
+        break;
+        case 8:
+            document.getElementById("negro").style.opacity= "1";
+        break;
+        case 9:
+            document.getElementById("morado").style.opacity= "1";
+        break;
+        case 10:
+            document.getElementById("cian").style.opacity= "1";
+        break;
+    }
+}
+
+function clickar() {
+    iniciar();
+}
