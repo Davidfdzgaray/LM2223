fb8a7275aab0b6dfdd1ca66d3a80e59eee190f4f
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-04-14 12:09:38 +0200

    ty

--- 3TRIMESTRE/JQUERY/EJERCICIOS2/ejercicio6.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#contar").click(function(){
-		$("#nparrafos").text($("p").length);
-        $("#ntitulos").text($("h1").length);
-		$("#nimagenes").text($("img").length);
-		$("#nenlaces").text($("a").length);
-	});
-});
