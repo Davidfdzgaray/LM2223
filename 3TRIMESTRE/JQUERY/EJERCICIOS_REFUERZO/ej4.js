e52ecf74c4f93097ea37cd70532f9f02f47180f9
davidfdzgaray <davidfernandez04@iesamachado.org>
2023-05-02 20:41:59 +0200

    r

---
+++ 3TRIMESTRE/JQUERY/EJERCICIOS_REFUERZO/ej4.js
@@ -0,0 +1,14 @@
+$('button').click(function() {
+    //var nombre =  $("#nombre").val();
+    $.ajax({
+      url: "https://api.github.com/users/"+$("#nombre").val(),
+      dataType: "json",
+      type:"GET",
+      success: function(data) {
+        window.open("href", data.html_url);
+      },
+      error: function() {
+        $("#objetivo").text("Ha ocurrido un error al buscar el titulo indicado");
+      }
+    });
+  });
