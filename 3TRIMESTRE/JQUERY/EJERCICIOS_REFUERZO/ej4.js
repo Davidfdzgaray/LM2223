ab17e22a454cd978316f666488a330f488a40947
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-05-05 12:20:05 +0200

    e

--- 3TRIMESTRE/JQUERY/EJERCICIOS_REFUERZO/ej4.js
+++ 3TRIMESTRE/JQUERY/EJERCICIOS_REFUERZO/ej4.js
@@ -1,14 +1,20 @@
-$('button').click(function() {
-    //var nombre =  $("#nombre").val();
-    $.ajax({
-      url: "https://api.github.com/users/"+$("#nombre").val(),
-      dataType: "json",
-      type:"GET",
-      success: function(data) {
-        window.open("href", data.html_url);
-      },
-      error: function() {
-        $("#objetivo").text("Ha ocurrido un error al buscar el usuario indicado");
-      }
+ $(document).ready(function () {
+    $('#buscar').click(function () {
+        let nombre = $('#nombre').val();
+
+        // Llamada AJAX utilizando jQuery
+        $.ajax({
+            url: "https://api.github.com/users/"+nombre,
+            success: function (data) {
+                if (data.Response === "True") {
+                    $("#objetivo").text("Usuario encontrado");
+                } else {
+                    $('#resultado').html('<p>No se encontró el perfil.</p>');
+                }
+            },
+            error: function () {
+                $('#resultado').html('<p>Error al buscar el perfil.</p>');
+            },
+        });
     });
-  });
+});
