ee360a2aae7675c556cd8c4398d03eebc6955baf
davidfdzgaray <davidfernandez02@iesamachado.org>
2023-05-17 12:01:07 +0200

    GH

--- 3TRIMESTRE/JQUERY/MODELO_EXAMEN/ej2.js
+++ 3TRIMESTRE/JQUERY/MODELO_EXAMEN/ej2.js
@@ -1,44 +1,60 @@
 $(document).ready(function () {
     $('#btn-libro').click(function () {
+        $("#resultados").empty();
         let id = $('#codLibro').val();
-        console.log;
-        // Llamada AJAX utilizando jQuery
+
         $.ajax({
-            url: `https://gutendex.com/books?ids=${id}`,
-            success: function (data) {
-                $('#resultados').html(`
-                <h2>Título: ${data.title}</h2>
-                <p>Autores: ${data.authors}</p>
-                <img src="${data.image}" alt="">
-                <p>Descargar: ${data.text}</p>
-                <p>Nº Descargas: ${data.download_count}</p>
-                `);
+            type: "GET",
+            url: "https://gutendex.com/books/"+id,
+            contentType: "application/json",
+            success: function (data){
+                let titulo = data.title;
+                let autor = data.authors[0].name;
+                let imagen = data.formats['image/jpeg'];
+                let enlace = data.formats['text/plain; charset=us-ascii'];
+                let numeroDescargas = data.download_count;
+                $("#resultados").empty();
+                $('#resultados').append(
+                '<p>'+'<img width="120" src="'+imagen+'"></p>' +
+                '<p>Título: '+titulo+'</p>' + 
+                '<p>Autor: '+autor+'</p>' + 
+                '<p><a href="'+enlace+'" target="_blank">Descarga aquí</a></p>' + 
+                '<p>Nº descargas: ' + numeroDescargas + '</p>');
             },
-            error: function () {
-                $('#resultados').html('ERROR');
-            },
+            error: function (){
+                alert('NO HAY RESULTADOS PARA SU BÚSQUEDA');
+            }
         });
     });
-});         
 
-$(document).ready(function () {
     $('#btn-todos').click(function () {
-        console.log;
-        // Llamada AJAX utilizando jQuery
+        $("#resultados").empty();
+
         $.ajax({
-            url: `https://gutendex.com/books`,
-            success: function (data) {
-                $('#resultados').html(`
-                <h2>Título: ${data.title}</h2>
-                <p>Autores: ${data.authors}</p>
-                <img src="${data.image}" alt="">
-                <p>Descargar: ${data.text}</p>
-                <p>Nº Descargas: ${data.download_count}</p>
-                `);
+            type: "GET",
+            url: "https://gutendex.com/books/",
+            contentType: "application/json",
+            success: function (data){
+                let num_libros = data.results.length;
+                for (let i=0; i<num_libros; i++){
+                    let respuesta = new Object(data.results[i]);
+                    let titulo = respuesta.title;
+                    let autor = respuesta.authors.length == 0 ? "Anónimo" : respuesta.authors[0].name;
+                    let imagen = respuesta.formats['image/jpeg'];
+                    let enlace = respuesta.formats['text/plain; charset=us-ascii'];
+                    let numeroDescargas = respuesta.download_count;
+                    $('#resultados').append(
+                    '<p>'+'<img width="120" src="'+imagen+'"></p>' +
+                    '<p>Título: '+titulo+'</p>' + 
+                    '<p>Autor: '+autor+'</p>' + 
+                    '<p><a href="'+enlace+'" target="_blank">Descarga aquí</a></p>' + 
+                    '<p>Nº descargas: ' + numeroDescargas + '</p>');
+                }
             },
-            error: function () {
-                $('#resultados').html('ERROR');
-            },
+            error: function (){
+                alert('ERROR');
+            }
         });
+        
     });
 });  
